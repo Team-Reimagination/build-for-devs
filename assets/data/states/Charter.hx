41ca8928541261251512e8e69587ca24901e4ff3
Frakist <90983918+Frakist@users.noreply.github.com>
2023-09-29 22:47:41 +0300

    new downscroll charter

--- assets/data/states/Charter.hx
+++ assets/data/states/Charter.hx
@@ -1,4 +1,45 @@
 import funkin.game.HudCamera;
+import flixel.FlxObject;
+var gridCamera = new FlxCamera();
+var scrollBarCamera = new FlxCamera();
 function postCreate() {
 	charterCamera = new HudCamera();
+	charterCamera.downscroll = true;
+	scrollBarCamera.height += topMenuSpr.bHeight;
+	charterCamera.y = topMenuSpr.bHeight;
+	gridCamera.y = topMenuSpr.bHeight;
+	FlxG.cameras.add(gridCamera, false);
+	FlxG.cameras.add(charterCamera, false);
+	FlxG.cameras.remove(uiCamera, false);
+	FlxG.cameras.add(uiCamera, false);
+	FlxG.cameras.add(scrollBarCamera, false);
+	charterCamera.bgColor = 0;
+	gridCamera.bgColor = 0;
+	scrollBarCamera.bgColor = 0;
+	gridCamera.flashSprite.scaleY = -1;
+	scrollBarCamera.flashSprite.scaleY = -1;
+	notesGroup.camera = eventsGroup.camera = selectionBox.camera = strumlineAddButton.camera = strumlineLockButton.camera = strumlineInfoBG.camera = strumLines.camera = charterCamera;
+	addEventSpr.cameras = [charterCamera];
+	gridBackdrops.cameras = eventsBackdrop.cameras = charterBG.cameras = [gridCamera];
+	charterBG.zoomFactor = 1;
+	scrollBar.cameras = [scrollBarCamera];
+	scrollBar.scrollFactor.set(0, 1);
+}
+var __lastMouse:Float = 0;
+function preUpdate(elapsed) {
+	__lastMouse = FlxG.mouse._globalScreenY;
+	if (FlxG.mouse.screenY > 25)
+		FlxG.mouse._globalScreenY = 770 - FlxG.mouse._globalScreenY;
+	if (true) {
+		__crochet = ((60 / Conductor.bpm) * 1000);
+		if (!FlxG.keys.pressed.CONTROL)
+			Conductor.songPosition += ((__crochet * FlxG.mouse.wheel) - Conductor.songOffset) * 2;
+	}
+}
+function postUpdate(elapsed) {
+	for (i in notesGroup.members)
+		if (i.flipY != true) i.flipY = true;
+	FlxG.mouse._globalScreenY = __lastMouse;
+	gridCamera.scroll = charterCamera.scroll;
+	gridCamera.zoom = charterCamera.zoom;
 }
