a63e4d41f26362f7fbdd48d42ee3f501500372c6
Frakist <90983918+Frakist@users.noreply.github.com>
2024-02-27 20:58:29 +0200

    new (unbroken) build

--- addons/Widescreen Support/data/global.hx
+++
@@ -1,5 +0,0 @@
-function update() {
-	FlxG.game.resizeGame(1280 * ((FlxG.stage.window.width / FlxG.stage.window.height) - (16/9) + 1), 720);
-	FlxG.scaleMode.width = 1280 * ((FlxG.stage.window.width / FlxG.stage.window.height) - (16/9) + 1);
-	FlxG.scaleMode.height = 720;
-}
--- addons/Widescreen Support/songs/fixCam.hx
+++
@@ -1,4 +0,0 @@
-function postCreate() {
-	var widththing = 1280 * ((FlxG.stage.window.width / FlxG.stage.window.height) - (16/9) + 1);
-	camHUD.x = (widththing / 2) - (640);
-}
